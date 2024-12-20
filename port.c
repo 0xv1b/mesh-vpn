f4fb5fe73735e9317a6fc037425e8f31ecd91c8d
0xv1b <vibhorsingh.1611@gmail.com>
2024-12-20 11:32:29 +0100

    did more

--- port.c
+++ port.c
@@ -29,21 +29,25 @@ int main(int argc, char const *argv[])
     const char *server_ip_str = argv[1];
     int server_port = atoi(argv[2]);
 
+    // Initialise the port
     struct vport_t vport;
     vport_init(&vport, server_ip_str, server_port);
 
+    // Create upforwarding thread that runs forward_ether_data_to_vswitch
     pthread_t up_forwarder;
     if(pthread_create(&up_forwarder, NULL, forward_ether_data_to_vswitch, &vport) != 0)
     {
         ERROR_PRINT_THEN_EXIT("fail to pthread_create: %s\n", strerror(errno));
     }
 
+    // Create downforwarding thread that runs forward_ether_data_to_vtap
     pthread_t down_forwarder;
     if(pthread_create(&down_forwarder, NULL, forward_ether_data_to_tap, &vport) != 0)
     {
         ERROR_PRINT_THEN_EXIT("fail to pthread_create: %s\n", strerror(errno));
     }
 
+    // Wait for threads to finish
     if(pthread_join(up_forwarder, NULL) != 0 || pthread_join(down_forwarder, NULL) != 0)
     {
         ERROR_PRINT_THEN_EXIT("fail to pthread_join: %s\n", strerror(errno));
@@ -54,11 +58,52 @@ int main(int argc, char const *argv[])
 }
 
 
-void vport_init(struct vport_t *vport, const char *server_ip_str, int server_port){}
+void vport_init(struct vport_t *vport, const char *server_ip_str, int server_port)
+{
+    // TAP device
+    char ifname[IFNAMSIZ] = "tapuyan";
+    int tapfd = tap_alloc(ifname);
+    if (tapfd < 0)
+    {
+        ERROR_PRINT_THEN_EXIT("fail to tap_alloc: %s\n", strerror(errno));
+    }
 
-void *forward_ether_data_to_tap(void *raw_vport) {}
+    // Socket
+    int vport_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (vport_sockfd < 0)
+    {
+        ERROR_PRINT_THEN_EXIT("fail to socket: %s\n", strerror(errno));
+    }
 
-void *forward_ether_data_to_vswitch(void *raw_vport) {}
+    // Vswitch info
+    struct sockaddr_in vswitch_addr;
+    memset(&vswitch_addr, 0, sizeof(vswitch_addr))
+    vswitch_addr.sin_family = AF_INET;
+    vswitch_addr.sin_port = htons(server_port);
+
+
+    if (inet_pton(AF_INET, server_ip_str, &vswitch_addr.sin_addr) != 1)
+    {
+        ERROR_PRINT_THEN_EXIT("fail to inet_pton: %s\n", strerror(errno));
+    }
+
+    vport->tapfd = tapfd;
+    vport->vport_sockfd = vport_sockfd;
+    vport->vswitch_addr = vswitch_addr;
+
+    printf("[VPort] TAP device name: %s, VSwitch: %s:%d\n", ifname, server_ip_str, server_port);
+
+}
+
+void *forward_ether_data_to_tap(void *raw_vport) 
+{
+
+}
+
+void *forward_ether_data_to_vswitch(void *raw_vport) 
+{
+    
+}
 
 
 
